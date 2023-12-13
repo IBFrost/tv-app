bd3cc03e6198a72d2f35e81acd479f44a0d040e0
IBFrost <126430979+IBFrost@users.noreply.github.com>
2023-12-13 14:56:31 -0500

    Updating to present
    Previous work had been done directly to another repo to resolve some appearance issues among other things.
    This was roughly the progress as of 12/7.

--- src/tv-app.js
+++ src/tv-app.js
@@ -2,6 +2,7 @@
 import { LitElement, html, css } from "lit";
 import "@shoelace-style/shoelace/dist/components/dialog/dialog.js";
 import "@shoelace-style/shoelace/dist/components/button/button.js";
+import "@lrnwebcomponents/video-player/video-player.js";
 import "./tv-channel.js";
 
 export class TvApp extends LitElement {
@@ -11,6 +12,20 @@ export class TvApp extends LitElement {
     this.name = "";
     this.source = new URL("../assets/channels.json", import.meta.url).href;
     this.listings = [];
+    this.activeItem = {
+      id: null,
+      title: null,
+      presenter: null,
+      description: null,
+      video: null,
+    };
+    this.nextItem = {
+      id: null,
+      title: null,
+      presenter: null,
+      description: null,
+      video: null,
+    };
   }
   // convention I enjoy using to define the tag's name
   static get tag() {
@@ -22,82 +37,217 @@ export class TvApp extends LitElement {
       name: { type: String },
       source: { type: String },
       listings: { type: Array },
+      activeItem: { type: Object },
+      nextItem: { type: Object },
     };
   }
   // LitElement convention for applying styles JUST to our element
   static get styles() {
     return [
       css`
-      :host {
-        display: block;
-        margin: 16px;
-        padding: 16px;  
-      }
+        :host {
+          display: block;
+        }
 
-      .listing-container {
-        justify-self: center;
-        max-width: 1344px ;
-        justify-items: left;
-        display: flex;
-        float: none;
-        flex-direction: row;
-        flex-grow: 1;
-        flex-wrap: nowrap;
-        overflow-x: auto;
-        overflow-y: auto;
-        padding-left: .5rem;
-        padding-right: .5rem;
-        text-rendering: optimizeLegibility;
-        width: 100%;
-        margin: 0 auto;
-        position: relative;
-        animation-delay: 1s;
-        animation-duration: 1s;
-        line-height: 1.5;
-        font-size: 1em;
-      }
+        header {
+          color: #000;
+          padding: 16px;
+          text-align: center;
+        }
 
-      .
+        .h1 {
+          font-size: 32px;
+          margin-bottom: 16px;
+        }
+
+        .channel-container {
+          margin-left: 16px;
+          margin-right: 16px;
+          max-width: 100%;
+          display: flex;
+          flex-direction: row;
+          flex-grow: 1;
+          flex-wrap: nowrap;
+          overflow-x: auto;
+          overflow-y: auto;
+        }
+
+        .main-content {
+          display: flex;
+          flex-direction: row;
+          flex-wrap: wrap;
+          margin: 12px;
+        }
+
+        .player-container {
+          border-radius: 8px;
+          padding: 12px;
+          flex-grow: 10;
+          flex-basis: calc(66% - 32px); /* 66% minus padding on both sides */
+          flex-shrink: 3;
+          min-width: 445px;
+          min-height: 250px;
+        }
+
+        .player {
+          width: 100%;
+          aspect-ratio: 16/9;
+          border-radius: 8px;
+        }
+
+        .discord {
+          display: flex;
+          padding: 12px;
+          flex-grow: 10; /* Adjust the flex-grow value */
+          min-width: 0; /* Allow the box to shrink beyond its content */
+          flex-shrink: 2;
+          min-height: 300px;
+        }
+
+        .discord widgetbot {
+          overflow: hidden;
+          background-color: rgb(54, 57, 62);
+          border-radius: 8px;
+          width: 100%;
+          height: 100%;
+          vertical-align: top;
+        }
+
+        .discord iframe {
+          border-radius: 8px;
+          border: none;
+          width: inherit;
+          height: 100%;
+        }
+        .video-descritpion {
+          flex-grow: 10;
+        }
       `,
     ];
   }
   // LitElement rendering template of your element
   render() {
     return html`
-      <h2>${this.name}</h2>
-      <div class="listing-container">
+      <header>
+        <h1>${this.name}</h1>
+      </header>
+
+      <div class="channel-container">
         ${this.listings.map(
           (item) => html`
             <tv-channel
+              id="${item.id}"
               title="${item.title}"
               presenter="${item.metadata.author}"
+              description="${item.description}"
+              video="${item.metadata.source}"
               @click="${this.itemClick}"
             >
             </tv-channel>
           `
         )}
       </div>
-      <div>
-        <!-- video -->
+      <div class="main-content">
+        <div class="player-container">
+          <!-- video -->
+
+          <video-player
+            class="player"
+            source="${this.createSource()}"
+            accent-color="orange"
+            dark
+            track="https://haxtheweb.org/files/HAXshort.vtt"
+          >
+          </video-player>
+          <div class="video-description">
+            <tv-channel
+              title="${this.activeItem.title}"
+              presenter="${this.activeItem.presenter}"
+              ><p>${this.activeItem.description}</p></tv-channel
+            >
+          </div>
+        </div>
+
         <!-- discord / chat - optional -->
+        <div class="discord">
+          <widgetbot
+            server="954008116800938044"
+            channel="1106691466274803723"
+            width="100%"
+            height="100%"
+            ><iframe
+              title="WidgetBot Discord chat embed"
+              allow="clipboard-write; fullscreen"
+              src="https://e.widgetbot.io/channels/954008116800938044/1106691466274803723?api=a45a80a7-e7cf-4a79-8414-49ca31324752"
+            ></iframe
+          ></widgetbot>
+          <script src="https://cdn.jsdelivr.net/npm/@widgetbot/html-embed"></script>
+        </div>
       </div>
       <!-- dialog -->
-      <sl-dialog label="Dialog" class="dialog">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        <sl-button slot="footer" variant="primary" @click="${this.closeDialog}"
-          >Close</sl-button
+      <sl-dialog label=" ${this.nextItem.title}" class="dialog">
+        <p class="dialog-description">
+          ${this.nextItem.presenter} | ${this.nextItem.description}
+        </p>
+        <sl-button slot="footer" variant="primary" @click="${this.watchVideo}"
+          >Watch</sl-button
         >
       </sl-dialog>
     `;
   }
 
-  closeDialog(e) {
+  changeVideo() {
+    // Update the iframe source URL when an item is clicked
+    const iframe = this.shadowRoot.querySelector('video-player').querySelector("iframe");
+    iframe.src = this.createSource();
+    
+    this.shadowRoot.querySelector('video-player').shadowRoot.querySelector('a11y-media-player').play()
+  }
+
+  extractVideoID(link) {
+    try {
+      const url = new URL(link);
+      const searchParams = new URLSearchParams(url.search);
+      return searchParams.get("v");
+    } catch (error) {
+      console.error("Invalid URL:", link);
+      return null;
+    }
+  }
+
+  createSource() {
+    return (
+      "https://www.youtube.com/embed/" +
+      this.extractVideoID(this.activeItem.video)
+    );
+  }
+
+  closeDialog() {
     const dialog = this.shadowRoot.querySelector(".dialog");
     dialog.hide();
   }
 
+  watchVideo() {
+    this.activeItem = {
+      id: this.nextItem.id,
+      title: this.nextItem.title,
+      presenter: this.nextItem.presenter,
+      description: this.nextItem.description,
+      video: this.nextItem.video,
+    };
+    this.closeDialog();
+    this.changeVideo();
+    
+  }
+
   itemClick(e) {
-    console.log(e.target);
+    this.nextItem = {
+      id: e.target.id,
+      title: e.target.title,
+      presenter: e.target.presenter,
+      description: e.target.description,
+      video: e.target.video,
+    };
     const dialog = this.shadowRoot.querySelector(".dialog");
     dialog.show();
   }
@@ -128,5 +278,6 @@ export class TvApp extends LitElement {
       });
   }
 }
+
 // tell the browser about our tag and class it should run when it sees it
 customElements.define(TvApp.tag, TvApp);
--- src/tv-channel.js
+++ src/tv-channel.js
@@ -1,57 +1,56 @@
 // import stuff
-import { LitElement, html, css } from 'lit';
+import { LitElement, html, css } from "lit";
 
 export class TvChannel extends LitElement {
   // defaults
   constructor() {
     super();
-    this.title = '';
-    this.presenter = '';
+    this.title = "";
+    this.presenter = "";
   }
   // convention I enjoy using to define the tag's name
   static get tag() {
-    return 'tv-channel';
+    return "tv-channel";
   }
   // LitElement convention so we update render() when values change
   static get properties() {
     return {
       title: { type: String },
+      description: { type: String },
       presenter: { type: String },
+      video: { type: String }
     };
   }
   // LitElement convention for applying styles JUST to our element
   static get styles() {
     return css`
       :host {
-        text-rendering: optimizeLegibility;
-        box-sizing: inherit;
-        display: inline-block;
-        line-height: 1.2;
-        font-size: 1em;
-        font-weight: 400;
-        min-width: 300px;
-        margin: .5rem .5rem .25rem .5rem;
-        padding: 0;
-        transition: all 0.25s ease-in-out;
-        overflow: hidden;
-        text-overflow: ellipsis;
+        display: inline-flex;
+        flex-grow: 10;
       }
+
       .wrapper {
-        margin: .5rem;
-        padding: .5rem;
+        line-height: 4px;
+        min-width: 256px;
+        margin-right: 4px;
         padding-left: 16px;
         padding-right: 16px;
-        border-radius: 6px;
-        border-color: #4a4a4a;
-        box-shadow: 0px 0px 0px 1px #dbdbdb;
-        background-color: #ffffff;
+        border: 1px solid #2c2c2c;
+        border-radius: 8px;
       }
+
+      .wrapper h5 {
+        font-size: 1.125em;
+        margin-bottom: 0.8888em;
+        color: #363636;
+      }
+      
       p {
-        font-size: 12px;
+        font-size: 1em;
       }
-      h5 {
-        margin: 0;
-        font-weight: 400;
+
+      ::slotted(p) {
+        line-height: 32px;
       }
 
     `;
